d6afa5942ad95e6579718d15c581a084f9993ec7
Pau Vilella <pau.vilella@apple.com>
2025-02-26 18:01:46 +0100

    Add pauvilella user to be managed by this config and add crossplane user

--- aws/iam/users.tf
+++ aws/iam/users.tf
@@ -1,7 +1,24 @@
+# Main pauvilella IAM user (my user)
+# Also used for doing the terragrunt commands in this repo.
+# It has been imported into this config
+resource "aws_iam_user" "pauvilella" {
+  name          = "pauvilella"
+  path          = "/"
+  force_destroy = false
+}
+resource "aws_iam_user_policy_attachment" "pauvilella_attachment1" {
+  user       = aws_iam_user.pauvilella.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 # User for the crossplane-poc repo (used for creating
 # aws resources from Crossplane)
-resource "aws_iam_user" "crossplane_poc" {
-  name          = "crossplane-poc"
+resource "aws_iam_user" "crossplane" {
+  name          = "crossplane"
   path          = "/"
   force_destroy = true
+}
+resource "aws_iam_user_policy_attachment" "crossplane_attachment1" {
+  user       = aws_iam_user.crossplane.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
